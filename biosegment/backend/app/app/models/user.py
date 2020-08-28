443c77f6eb5eb5f469accac535b267a628447c5c
Benjamin Rombaut <Benjamin.Rombaut@UGent.be>
2020-08-28 12:41:11 +0200

    add projects to user

--- biosegment/backend/app/app/models/user.py
+++ biosegment/backend/app/app/models/user.py
@@ -7,6 +7,7 @@ from app.db.base_class import Base
 
 if TYPE_CHECKING:
     from .item import Item  # noqa: F401
+    from .project import Project  # noqa: F401
 
 
 class User(Base):
@@ -17,3 +18,4 @@ class User(Base):
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
     items = relationship("Item", back_populates="owner")
+    projects = relationship("Project", back_populates="owner")
