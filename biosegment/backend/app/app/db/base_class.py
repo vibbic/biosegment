d7c62d527b59bc837aa7c7c3ee8e951739b812e7
Benjamin Rombaut <Benjamin.Rombaut@UGent.be>
2020-08-28 11:58:40 +0200

    setup poetry dev env

--- biosegment/backend/app/app/db/base_class.py
+++ biosegment/backend/app/app/db/base_class.py
@@ -2,7 +2,6 @@ from typing import Any
 
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
-
 @as_declarative()
 class Base:
     id: Any
--- biosegment/backend/app/scripts/lint.sh
+++ biosegment/backend/app/scripts/lint.sh
@@ -5,4 +5,4 @@ set -x
 mypy app
 black app --check
 isort --recursive --check-only app
-flake8
+flake8 --exclude .git,__pycache__,.env,.mypy_cache --per-file-ignores="__init__.py:F401"
