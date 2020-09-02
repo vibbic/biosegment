2a878ae16b2a8ad529ccb552320a370f2b4a42c1
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2020-09-02 17:02:19 +0200

    clean up gpu code in backend

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -4,5 +4,4 @@ celery_app = Celery("worker", broker="amqp://guest@queue//")
 
 celery_app.conf.task_routes = {
     "app.worker.test_celery": "main-queue",
-    "app.worker.test_pytorch": "main-queue",
 }
