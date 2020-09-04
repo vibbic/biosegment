77b1647e6b8137c5202435eae0f49039f677830b
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2020-09-04 14:14:35 +0200

    correct syntax to ignore __init__

--- backend/app/scripts/format.sh
+++ backend/app/scripts/format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place app --exclude .git,__pycache__,.env,.mypy_cache --per-file-ignores="__init__.py:F401"
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place app --exclude .git,__pycache__,.env,.mypy_cache,__init__.py
 black app
 isort --recursive --apply app
