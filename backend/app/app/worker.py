03c64d26741e66f6c475990ce672c85c11ac4d17
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2020-09-04 17:28:09 +0200

    lint

--- backend/app/app/worker.py
+++ backend/app/app/worker.py
@@ -9,8 +9,8 @@ client_sentry = Client(settings.SENTRY_DSN)
 @celery_app.task(bind=True, acks_late=True)
 def test_celery(self, timeout: int) -> str:
     from time import sleep
+
     for i in range(timeout):
         sleep(1)
-        self.update_state(state='PROGRESS',
-                meta={'current': i, 'total': timeout})
+        self.update_state(state="PROGRESS", meta={"current": i, "total": timeout})
     return f"test task return after timeout of {timeout} seconds"
