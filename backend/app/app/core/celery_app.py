40a00d7355956847042596d79d6684860eae669a
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2020-09-07 10:17:42 +0200

    add gpu names for queue and worker

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -6,7 +6,7 @@ celery_app.conf.result_backend = "redis://queue:6379/0"
 
 celery_app.conf.task_routes = {
     "app.worker.test_celery": "main-queue",
-    "app.worker.test_pytorch": "main-queue",
-    "app.worker.infer_unet2d": "main-queue",
-    "app.worker.train_unet2d": "main-queue",
+    "app.worker.test_pytorch": "gpu-queue",
+    "app.worker.infer_unet2d": "gpu-queue",
+    "app.worker.train_unet2d": "gpu-queue",
 }
