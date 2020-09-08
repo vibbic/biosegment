c573936f073f3d7f2408c32293d2ae69c02db7dd
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2020-09-08 12:40:43 +0200

    better base api implementation

--- dash_frontend/app/api/base.py
+++ dash_frontend/app/api/base.py
@@ -4,9 +4,29 @@ import requests
 import logging
 import http.client
 
+from requests_toolbelt import sessions
+
 # from requests_oauthlib import OAuth2Session
 from app.env import API_DOMAIN
+from app.api.TimeoutHTTPAdapter import TimeoutHTTPAdapter
 
+# TODO remove credentials and use client secret
+ROOT_USERNAME = "admin@biosegment.irc.ugent.be"
+ROOT_PASSWORD = "m1cr0scopy"
+# CLIENT_ID = "<your client key>"
+# CLIENT_SECRET = "<your client secret>"
+
+API_ROOT = f'http://{API_DOMAIN}/api/v1/'
+TOKEN_URL = 'login/access-token'
+
+http_session = sessions.BaseUrlSession(base_url=API_ROOT)
+
+# Mount it for both http and https usage
+adapter = TimeoutHTTPAdapter(timeout=2.5)
+http_session.mount("https://", adapter)
+http_session.mount("http://", adapter)
+
+# setup logging
 http.client.HTTPConnection.debuglevel = 1
 
 logging.basicConfig()
@@ -15,16 +35,8 @@ requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
-# TODO remove credentials and use client secret
-ROOT_USERNAME = "admin@biosegment.irc.ugent.be"
-ROOT_PASSWORD = "m1cr0scopy"
-# CLIENT_ID = "<your client key>"
-# CLIENT_SECRET = "<your client secret>"
-
-API_ROOT = f"http://{API_DOMAIN}/api/v1/"
-TOKEN_URL = f'{API_ROOT}login/access-token'
-
 def get_tokens():
+    global http_session
     # TODO refresh tokens
     data = {
         'grant_type': 'password',
@@ -47,7 +59,7 @@ def get_tokens():
     logging.debug(data)
     logging.debug(TOKEN_URL)
     try:
-        access_token_response = requests.post(TOKEN_URL, data=data, timeout=1,
+        access_token_response = http_session.post(TOKEN_URL, data=data, timeout=1,
         # auth=(client_id, client_secret)
         )
         logging.debug(access_token_response)
@@ -60,19 +72,23 @@ def get_tokens():
         token = tokens['access_token']
         logging.debug(f"Token type: {type(token)}")
     except Exception as e:
-        logging.debug(f"Error {e}")
-        token = None
+        logging.error(f"Error {e}")
+        raise e
     return str(token)
     
+
 
 token = get_tokens()
 
 def get(path, headers={}, **kwargs):
     global token
-    logging.debug(f"GET using token {token}")
-    logging.debug(f"Path {path}")
-    r = requests.get(
-        f"{API_ROOT}{path}", 
+    global http_session
+    if not token:
+        token = get_tokens()
+    # logging.debug(f"GET using token {token}")
+    # logging.debug(f"Path {path}")
+    r = http_session.get(
+        path, 
         headers={
             'Authorization': 'Bearer ' + token,
             'Accept': 'application/json',
@@ -81,20 +97,23 @@ def get(path, headers={}, **kwargs):
         timeout=1
     )
     assert r.status_code == 200
-    logging.debug(f"f {r}")
+    # logging.debug(f"f {r}")
     return r.json()
 
 def post(path, headers={}, **kwargs):
     global token
-    logging.debug(f"POST using token {token}")
-    logging.debug(f"Path {path}")
+    global http_session
+    if not token:
+        token = get_tokens()
+    # logging.debug(f"POST using token {token}")
+    # logging.debug(f"Path {path}")
     try:
         payload = kwargs["json"]
     except:
         payload = None
-    logging.debug(f"JSON {payload}")
-    r = requests.post(
-        f"{API_ROOT}{path}", 
+    # logging.debug(f"JSON {payload}")
+    r = http_session.post(
+        path, 
         headers={
             'Authorization': 'Bearer ' + token, 
             # **headers
@@ -105,6 +124,6 @@ def post(path, headers={}, **kwargs):
         json = payload
     )
     assert r.status_code == 200 or r.status_code == 201
-    logging.debug(f"f {r}")
+    # logging.debug(f"f {r}")
     return r.json()
 
