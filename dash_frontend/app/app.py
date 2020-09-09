8efd4d5abd9471dc82ae41261f58dda7b6e7261c
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2020-09-09 09:40:10 +0200

    remove updating...

--- dash_frontend/app/app.py
+++ dash_frontend/app/app.py
@@ -6,7 +6,9 @@ external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = dash.Dash(
     __name__, 
     external_stylesheets=external_stylesheets,
-    url_base_pathname='/dash/'
+    url_base_pathname='/dash/',
+    # remove Updating... message
+    update_title=None,
 )
 server = app.server
 app.config.suppress_callback_exceptions=True
