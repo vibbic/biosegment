d65aefa2351abd2de7516730de4c1b1b0188a446
Frank Vernaillen <frank.vernaillen@ugent.be>
2020-10-01 12:28:56 +0200

    Use same celery options on Windows as on Linux.

--- gpu_worker/start_worker.bat
+++ gpu_worker/start_worker.bat
@@ -1,4 +1,4 @@
 @rem TODO Add watchmedo functionality like in start_worker.sh
 @echo Please ensure that the ROOT_DATA_FOLDER environment variable is set!
 @echo ROOT_DATA_FOLDER=%ROOT_DATA_FOLDER%
-celery -A app.worker  worker
+celery worker -A app.worker -l info -Q gpu-queue -n gpu_worker@%h
