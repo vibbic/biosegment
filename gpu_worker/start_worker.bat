4a706c4f7103e5a06dd05e8e8e35a05d0ba1de0f
Frank Vernaillen <frank.vernaillen@ugent.be>
2020-10-01 12:36:51 +0200

    Added comment about broken watchmedo on Windows

--- gpu_worker/start_worker.bat
+++ gpu_worker/start_worker.bat
@@ -1,4 +1,5 @@
-@rem TODO Add watchmedo functionality like in start_worker.sh
+@rem Note that the Unix version of this script (start_worker.sh) has auto-restart functionality.
+@rem On Windows we had to remove this feature since watchmedo auto-restart is broken on Windows (https://github.com/gorakhargosh/watchdog/issues/387)
 @echo Please ensure that the ROOT_DATA_FOLDER environment variable is set!
 @echo ROOT_DATA_FOLDER=%ROOT_DATA_FOLDER%
 celery worker -A app.worker -l info -Q gpu-queue -n gpu_worker@%h
