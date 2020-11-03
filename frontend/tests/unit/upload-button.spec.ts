bddc0c87f1a61290814898687ad9bcd266485478
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2020-11-03 10:19:02 +0100

    add correct test setup syntax

--- frontend/tests/unit/upload-button.spec.ts
+++ frontend/tests/unit/upload-button.spec.ts
@@ -1,11 +1,27 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import UploadButton from '@/components/UploadButton.vue';
-import '@/plugins/vuetify';
+import Vuetify from 'vuetify';
+
+const localVue = createLocalVue()
 
 describe('UploadButton.vue', () => {
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  const mountFunction = options => {
+    return shallowMount(UploadButton, {
+      localVue,
+      vuetify,
+      ...options,
+    })
+  }
+
   it('renders props.title when passed', () => {
     const title = 'upload a file';
-    const wrapper = shallowMount(UploadButton, {
+    const wrapper = mountFunction({
       slots: {
         default: title,
       },
