67240034cafc56fb112ce3976a224db3179e8811
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2020-11-17 15:29:15 +0100

    add deepCopy and filterUndefined

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -3,3 +3,12 @@ export const getLocalToken = () => localStorage.getItem('token');
 export const saveLocalToken = (token: string) => localStorage.setItem('token', token);
 
 export const removeLocalToken = () => localStorage.removeItem('token');
+
+export function filterUndefined(obj) {
+    Object.keys(obj).forEach((key) => obj[key] === undefined && delete obj[key]); 
+    return obj;
+}
+
+export function deepCopy(obj) {
+    return JSON.parse(JSON.stringify(obj))
+}
