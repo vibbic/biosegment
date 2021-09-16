8bfd3f8c770f24289fbefe00ba4efb8a5501fa28
Benjamin.Rombaut <benjaminr@cp0293.irc.ugent.be>
2021-09-16 16:25:10 +0200

    set broker and result via script

--- gpu_worker/app/celery_app.py
+++ gpu_worker/app/celery_app.py
@@ -1,9 +1,3 @@
 from celery import Celery
 
-celery_app = Celery("worker", broker="redis://localhost:6379/0")
-celery_app.conf.result_backend = 'redis://localhost:6379/0'
-# configure celery only via backend code
-# celery_app.conf.task_routes = {
-#     "app.worker.test_pytorch": "main-queue",
-#     "app.worker.infer": "main-queue",
-# }
+celery_app = Celery("worker")
